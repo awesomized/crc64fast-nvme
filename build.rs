fd9b4b131387672e83d20cf55b5a2c0ff9fb3a3e
Landon James <lnj@amazon.com>
2025-01-18 21:42:16 -0800

    Add new c-bindings feature
    Feature optionally enables building the c bindings. It is off by default.

--- build.rs
+++ build.rs
@@ -1,11 +1,13 @@
 // build.rs
 
-extern crate cbindgen;
-
 fn main() {
-    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    #[cfg(feature = "c-bindings")]
+    {
+        extern crate cbindgen;
+        let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::generate(crate_dir)
-        .expect("Unable to generate C bindings.")
-        .write_to_file("crc64fast_nvme.h");
+        cbindgen::generate(crate_dir)
+            .expect("Unable to generate C bindings.")
+            .write_to_file("crc64fast_nvme.h");
+    }
 }
